dcc2c626a302e4537ed0e8e5330217e37a97e731
Adnan <adnan_abbasi@yahoo.com>
2020-12-09 16:55:56 -0500

    End of Custom Error Handling

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const BootcampSchema = new mongoose.Schema(
   {
@@ -96,17 +97,13 @@ const BootcampSchema = new mongoose.Schema(
     createdAt: {
       type: Date,
       default: Date.now
-    },
-    user: {
-      type: mongoose.Schema.ObjectId,
-      ref: 'User',
-      required: true
     }
-  },
-  {
-    toJSON: { virtuals: true },
-    toObject: { virtuals: true }
-  }
-);
+  });
+
+  // Create bootcamp slug from the name
+  BootcampSchema.pre('save', function(next) {
+    this.slug = slugify(this.name, { lower: true });
+    next();
+  });
 
 module.exports = mongoose.model('Bootcamp', BootcampSchema);
